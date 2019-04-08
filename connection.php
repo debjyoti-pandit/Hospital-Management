87b0c4985a4cfc59e9fa092c140c8d85281dd561
Debjyoti Pandit <debjyotipandit35@gmail.com>
2019-04-09 00:04:19 +0530

    changes connection

--- connection.php
+++ connection.php
@@ -1,3 +1,7 @@
 <?php
-	$con=mysqli_connect("localhost:3306","admin","admin","hospital") or die("can't connect to database");
+	$host        = "host = ec2-23-21-136-232.compute-1.amazonaws.com";
+	$port        = "port = 5432";
+	$dbname      = "dbname = d48hdnq2naa5rn";
+	$credentials = "user = jbydjsonlnrlsy password=6f66a1ad66ccafbc96a854b5e73be06ed8ba51ee10696c77320733375b2cba21";
+	$con=pg_connect( "$host $port $dbname $credentials"  ); or die("can't connect to database");
 ?>
