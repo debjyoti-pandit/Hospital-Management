7a99cc26f29292be8c2682e402ca5fc5162bd3c1
Debjyoti Pandit <debjyotipandit35@gmail.com>
2019-04-09 00:23:49 +0530

    changes connection

--- connection.php
+++ connection.php
@@ -1,7 +1,3 @@
 <?php
-	$host        = "host = ec2-23-21-136-232.compute-1.amazonaws.com";
-	$port        = "port = 5432";
-	$dbname      = "dbname = d48hdnq2naa5rn";
-	$credentials = "user = jbydjsonlnrlsy password=6f66a1ad66ccafbc96a854b5e73be06ed8ba51ee10696c77320733375b2cba21";
-	$con=pg_connect( "$host $port $dbname $credentials"  ); or die("can't connect to database");
+	$con=mysqli_connect("us-cdbr-iron-east-03.cleardb.net","b2b4d8e73c57b3","ac52015e","heroku_b49f621c0354349") or die("can't connect to database");
 ?>
